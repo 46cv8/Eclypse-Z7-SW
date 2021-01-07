9d2945b03474f308650e924c729c1804481a7692
46cv8 <58377031+46cv8@users.noreply.github.com>
2021-01-07 17:59:44 +0900

    improvements to test app

--- ZmodDAC1411_Demo_Linux/src/main.cpp
+++ ZmodDAC1411_Demo_Linux/src/main.cpp
@@ -12,6 +12,8 @@
 #include <string.h>
 #include <fstream>
 #include <iostream>
+#include <math.h>
+#include <sys/time.h>
 
 #include "zmod.h"
 #include "zmoddac1411.h"
@@ -33,70 +35,117 @@
 * @param amplitude - the amplitude for the generated ramp
 * @param targetLength - the samples to use for this ramp waveform
 * @param channel - the channel where samples will be generated
-* @param frequencyDivider - the output frequency divider
 * @param gain - the gain for the channel
 * @param cycles - the number of cycles to run the waveform
 */
-void dacRampDemo(ZMODDAC1411 *dacZmod, uint16_t *buf, float offset, float amplitude, size_t length, uint8_t channel, uint8_t frequencyDivider, uint8_t gain)
+void dacRampDemo(ZMODDAC1411 *dacZmod, uint16_t *buf, float offset, float amplitude, size_t length, uint8_t channel, uint8_t gain)
 {
-	float val;
-	uint16_t valRaw;
-	float step;
+//	float val;
+//	float step = amplitude/(length>>2);
+//	uint16_t valRaw;
 	uint32_t i;
+	int64_t valInt;
+	int64_t offsetInt = ((double)offset/(double)(gain ? 5.0:1.25))*(double)((int64_t)1<<(13+48));
+	int64_t amplitudeInt = ((double)amplitude/(double)(gain ? 5.0:1.25))*(double)((int64_t)1<<(13+48));
+	int64_t stepInt = amplitudeInt/(length>>2);
 
-	step = amplitude/(length>>2);
-	dacZmod->setOutputSampleFrequencyDivider(frequencyDivider);
 	dacZmod->setGain(channel, gain);
 
 	// ramp up
 	i = 0;
+////	val = -amplitude;
+//	for(; i < length>>1; i++)
+//	{
+//		val = -amplitude + (i*amplitude)/(length>>2);
+//		valRaw = dacZmod->getSignedRawFromVolt(val + offset, gain);
+//		buf[i] = valRaw;
+////		val += step;
+//	}
+//	// ramp down
+////	val = amplitude;
+//	for(; i < length; i++)
+//	{
+//		val = amplitude - ((i-(length>>1))*amplitude)/(length>>2);
+//		valRaw = dacZmod->getSignedRawFromVolt(val + offset, gain);
+//		buf[i] = valRaw;
+////		val -= step;
+//	}
+	valInt = -amplitudeInt+offsetInt;
+	if (valInt < (int64_t)-0x1FFF << 48) {
+		valInt = (int64_t)-0x1FFF << 48;
+	}
 	for(; i < length>>1; i++)
 	{
-		val = -amplitude + (i*amplitude)/(length>>2);
-		valRaw = dacZmod->getSignedRawFromVolt(val + offset, gain);
-		buf[i] = valRaw;
+		if (i < 5 || i > (length>>1)-5) {
+			std::cout << "\nramp up: " <<  (int16_t)(valInt>>48) << " " << i;
+		}
+		buf[i] = ((uint16_t)((uint16_t)(valInt>>48) & 0x3FFF)) << 2;
+		valInt += stepInt;
 	}
 	// ramp down
+	valInt = amplitudeInt+offsetInt;
+	if (valInt > (int64_t)0x1FFF << 48) {
+		valInt = (int64_t)0x1FFF << 48;
+	}
 	for(; i < length; i++)
 	{
-		val = amplitude - ((i-(length>>1))*amplitude)/(length>>2);
-		valRaw = dacZmod->getSignedRawFromVolt(val + offset, gain);
-		buf[i] = valRaw;
+		if (i < (length>>1)+5 || i > length-5) {
+			std::cout << "\nramp down: " << (int16_t)(valInt>>48) << " " << i;
+		}
+		buf[i] = ((uint16_t)((uint16_t)(valInt>>48) & 0x3FFF)) << 2;;
+		valInt -= stepInt;
 	}
+
+	std::cout << "\nchannel: ";
+	std::cout << (int)channel;
 	std::cout << "\nused length: ";
 	std::cout << length;
-	std::cout << "\nused step: ";
-	std::cout << step;
+	std::cout << "\nused amplitudeInt: ";
+	std::cout << amplitudeInt;
+	std::cout << "\nused offsetInt: ";
+	std::cout << offsetInt;
+	std::cout << "\nused stepInt: ";
+	std::cout << stepInt;
 	std::cout << "\nlast i: ";
 	std::cout << i;
-	std::cout << "\nlast value: ";
-	std::cout << val;
-	std::cout << "\nlast value raw: ";
-	std::cout << valRaw;
+	std::cout << "\nlast valInt: ";
+	std::cout << valInt;
 }
 
 int main() {
 	std::cout << "\nZmodDAC1411 Demo Started";
 
 	ZMODDAC1411 dacZmod(DAC_BASE_ADDR, DAC_DMA_CH1_BASE_ADDR, DAC_DMA_CH2_BASE_ADDR, IIC_BASE_ADDR, DAC_FLASH_ADDR, DAC_DMA_CH1_IRQ, DAC_DMA_CH2_IRQ);
-	size_t lengthCh1 = 20000000;
-	size_t lengthCh2 = 15000000;
+	size_t lengthCh1 = 0x10000000; // 0x10000000
+	size_t lengthCh2 = 40000; // 45000000
 	uint16_t *bufCh1 = dacZmod.allocBuffer(0, lengthCh1);
 	uint16_t *bufCh2 = dacZmod.allocBuffer(1, lengthCh2);
 	uint32_t currentCycleCh1 = 0;
 	uint32_t currentCycleCh2 = 0;
-	uint32_t maxCycles = 100;
+	uint32_t maxCycles = 100000;
+	struct timeval stop, start;
+
+	dacZmod.setOutputSampleFrequencyDivider(0);
 
 	// dacPtr
 	// bufPtr
 	// offset 					2 V
 	// amplitude 				3 V
 	// length 					20000000 Samples
-	// channel 					CH1
-	// Output Frequency Divider	2
+	// channel 					0 - CH1, 1 - CH2
 	// gain						HIGH - corresponds to HIGH input Range
-	dacRampDemo(&dacZmod, bufCh1, 2, 3, lengthCh1, 0, 0, 1);
-	dacRampDemo(&dacZmod, bufCh2, 2, 3, lengthCh2, 1, 0, 1);
+	gettimeofday(&start, NULL);
+	dacRampDemo(&dacZmod, bufCh1, 2, 3, lengthCh1, 0, 1);
+	gettimeofday(&stop, NULL);
+	std::cout << "\nCh1 Buffer Populated in ";
+	std::cout << ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+	gettimeofday(&start, NULL);
+	dacRampDemo(&dacZmod, bufCh2, 2, 3, lengthCh2, 1, 1);
+	gettimeofday(&stop, NULL);
+	std::cout << "\nCh2 Buffer Populated in ";
+	std::cout << ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+	std::cout << std::flush;
+
 	// send data to DAC and start the instrument
 	dacZmod.setData(0, bufCh1, lengthCh1);
 	dacZmod.setData(1, bufCh2, lengthCh2);
@@ -108,8 +157,8 @@ int main() {
 		if (dacZmod.isDMATransferComplete(0))
 		{
 			currentCycleCh1++;
-			std::cout << "\nch1 cycle completed: ";
-			std::cout << currentCycleCh1;
+//			std::cout << "\nch1 cycle completed: ";
+//			std::cout << currentCycleCh1;
 			if (currentCycleCh1 < maxCycles)
 			{
 				dacZmod.setData(0, bufCh1, lengthCh1);
@@ -118,8 +167,8 @@ int main() {
 		if (dacZmod.isDMATransferComplete(1))
 		{
 			currentCycleCh2++;
-			std::cout << "\nch2 cycle completed: ";
-			std::cout << currentCycleCh2;
+//			std::cout << "\nch2 cycle completed: ";
+//			std::cout << currentCycleCh2;
 			if (currentCycleCh2 < maxCycles)
 			{
 				dacZmod.setData(1, bufCh2, lengthCh2);
